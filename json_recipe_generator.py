bef407b5fb5c923dd3f981a1196c9e23279b3d7e
larryworm1127 <shilerong@gmail.com>
2018-05-15 18:10:55 -0400

    Initial commit

---
+++ json_recipe_generator.py
@@ -0,0 +1,12 @@
+"""
+This module will be generate a JSON file for recipe created by the user
+
+May 15, 2018
+By Larry Shi
+"""
+
+# general imports
+from json import dump
+from sys import path
+
+RECIPE_PATH = 
